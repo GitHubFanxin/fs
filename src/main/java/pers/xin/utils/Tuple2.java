4d91398e427016650f5f0b90906cae1686d61623
xin <fanxinchn@foxmail.com>
2018-06-12 17:10:29 +0800

    v1.0.3 add tuple2

---
+++ src/main/java/pers/xin/utils/Tuple2.java
@@ -0,0 +1,35 @@
+package pers.xin.utils;
+
+/**
+ * Created by xin on 2017/4/17.
+ */
+public class Tuple2<E, T> {
+    private E e;
+    private T t;
+
+    public Tuple2(E e, T t){
+        this.e = e;
+        this.t = t;
+    }
+
+    public E _1(){
+        return e;
+    }
+
+    public T _2(){
+        return t;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Tuple2<E,T> o = (Tuple2<E,T>)obj;
+        return this.e.equals(o.e)&&this.t.equals(o.t) ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + e +
+                ", " + t +
+                '}';
+    }
+}
